9643f72423dfe5588f55a323ca4867500796cb4d
Angga Suriana <anggasurianae65@gmail.com>
2025-08-01 17:49:12 +0700

    update workflow

--- web-application/backend/src/models/user.js
+++ web-application/backend/src/models/user.js
@@ -3,6 +3,10 @@
 const { Model } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    static associate(models) { }
+  }
+
   User.init({
     id: {
       type: DataTypes.INTEGER,
